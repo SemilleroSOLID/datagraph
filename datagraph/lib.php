2f5cdd717d1c67ea852386483935e1be68f7fa84
yimmy <yimmy.1701523504@ucaldas.edu.co>
2021-09-06 16:39:11 -0500

    Semillero solid primeras archivos para la instalacion en moodle

---
+++ datagraph/lib.php
@@ -0,0 +1,15 @@
+<?php 
+
+  /*  defined( 'MOODLE_INTERNAL') || die
+
+    function report_participation_extend_navigation_course($navigation, $course, $context) {
+        global $CFG, $OUTPUT;
+        if (has_capability('report/participation:view', $context)) {
+            $url = new moodle_url('/report/participation/index.php', array('id'=>$course->id));
+            $navigation->add(get_string('pluginname', 'report_participation'), $url, navigation_node::TYPE_SETTING, null, null, new pix_icon('i/report', ''));
+        }
+    }
+
+*/
+
+?>
---
+++ datagraph/version.php
@@ -0,0 +1,9 @@
+<?php 
+
+defined ('MOODLE_INTERNAL') || die;
+$plugin->component = 'report_datagraph';
+$plugin->version = 2021051900;
+$plugin->require = 2020061500; // requiere version 3.9 
+
+
+?>
